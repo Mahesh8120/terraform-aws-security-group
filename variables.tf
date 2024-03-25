cbae667dc0770d37a99d73b5c6ec07892b435395
Mahesh8120 <jonnalagaddamahi@gmail.com>
2024-03-25 12:13:49 +0530

    terraform

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "sg_name" {
+  
+}
+
+variable "project_name" {
+  
+}
+
+variable "environment" {
+  
+}
+
+variable "common_tags" {
+  
+}
+ 
+variable "sg_tags" {
+  
+}
+variable "description" {
+  
+}
+
+variable "vpc_id" {
+  
+}
+variable "ingress_rules" {
+ default = {}
+}
